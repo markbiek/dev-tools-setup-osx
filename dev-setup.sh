cde211209d4431fb7b77d869b58ba532b13a86eb
Mark Biek <mark.biek@automattic.com>
2022-09-09 11:26:37 -0400

    Bug fixes and cleanup
    - Added check to make sure we're running on OSX
    - Fixed places where we explicitly mentioned `~/.zshrc`
    - Added date to `$SHELLRC` backup file
    - Use nvm to installed node if it's available. Otherwise fallback to
      fnm.
    - Various tweaks recommended by shellcheck

--- dev-setup.sh
+++ dev-setup.sh
@@ -2,23 +2,37 @@
 
 function info(){
     echo -e "\x1B[34m $1 \x1B[0m"
-    if [ ! -z "${2}" ]; then
+    if [ -n "${2}" ]; then
     echo -e "\x1B[34m $($2) \x1B[0m"
     fi
 }
 function error(){
     echo -e "\x1B[31m $1 \x1B[0m"
-    if [ ! -z "${2}" ]; then
+    if [ -n "${2}" ]; then
     echo -e "\x1B[31m $($2) \x1B[0m"
     fi
 }
 function success(){
     echo -e "\x1B[32m $1 \x1B[0m"
-    if [ ! -z "${2}" ]; then
+    if [ -n "${2}" ]; then
     echo -e "\x1B[32m $($2) \x1B[0m"
     fi
 }
 
+UNAME=`uname -s`
+if [[ $UNAME -ne "Darwin" ]]; then
+	error "This script can only run on OSX."
+	exit 1
+fi
+
+###############################################################
+# curl
+if ! which curl > /dev/null; then
+	error "You must have the command-line tool 'curl' installed to use this script"
+	exit 1
+fi
+
+###############################################################
 # Determine if we're using bash or zsh
 if [[ $SHELL == '/bin/bash' ]]; then
 	SHELLRC=~/.bashrc
@@ -30,37 +44,25 @@ else
 fi
 
 # Backup the shell config
-cat $SHELLRC > "$SHELLRC.orig"
-
-###############################################################
-# curl
-
-which curl > /dev/null
-if [[ $? != 0 ]]; then
-	error "You must have the command-line tool 'curl' installed to use this script"
-	exit 1
-fi
+cat $SHELLRC > "$SHELLRC.orig.$(date "+%Y%m%d%H%M%S")"
 
 ###############################################################
 # Homebrew
-which brew > /dev/null
-if [[ $? != 0 ]]; then
+if ! which brew > /dev/null; then
 	info "Installing Homebrew (https://brew.sh/)"
 	info "Enter your password when prompted. Accept the defaults at all other prompts."
 
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-	which brew > /dev/null
-	if [[ $? != 0 ]]; then
+	if ! which brew > /dev/null; then
 		error "Homebrew installation failed!"
 		exit 1
 	fi
 
 	# Add /opt/homebrew/bin to $PATH if it's not there already
-	egrep "PATH=.*?\/opt\/homebrew\/bin" ~/.zshrc > /dev/null
-	if [[ $? != 0 ]]; then
+	if ! grep -E "PATH=.*?\/opt\/homebrew\/bin" $SHELLRC > /dev/null; then
 		echo 'export PATH="/opt/homebrew/bin:$PATH' >> $SHELLRC
-		. $SHELLRC
+		source $SHELLRC
 	fi
 else
 	info "Homebrew is already installed."
@@ -68,14 +70,12 @@ fi
 
 ###############################################################
 # git
-which git > /dev/null
-if [[ $? != 0 ]]; then
+if ! which git > /dev/null; then
 	info "Installing git"
 
 	brew install git
 
-	which git > /dev/null
-	if [[ $? != 0 ]]; then
+	if ! which git > /dev/null; then
 		error "git installation failed!"
 		exit 1
 	fi
@@ -86,55 +86,57 @@ fi
 ###############################################################
 # fnm (node/npm version manager)
 which fnm > /dev/null
-if [[ $? != 0 ]]; then
+HAS_FNM=$?
+which nvm > /dev/null
+HAS_NVM=$?
+
+if [[ $HAS_FNM != 0 && $HAS_NVM != 0 ]]; then
+	# If we don't have fnm or nvm, default to fnm
 	info "Installing fnm"
 
 	brew install fnm
 
-	which fnm > /dev/null
-	if [[ $? != 0 ]]; then
+	if ! which fnm > /dev/null; then
 		error "fnm installation failed!"
 		exit 1
 	fi
 
 	# Configure fnm for the shell
-	egrep "eval.*?fnm env --use-on-cd" ~/.zshrc > /dev/null
-	if [[ $? != 0 ]]; then
+	if ! grep -E "eval.*?fnm env --use-on-cd" $SHELLRC > /dev/null; then
 		echo 'eval "$(fnm env --use-on-cd)"' >> $SHELLRC
-		. $SHELLRC
+		source $SHELLRC
 	fi
-else
-	info "fnm is already installed."
+
+	info "Using fnm to install node 16"
+	fnm install v16 >> /dev/null 2>&1
+
+	if ! fnm list | grep -E v16 > /dev/null; then
+		error "Could not find node v16!"
+		exit 1
+	fi
+elif [[ $HAS_NVM == 0 ]]; then
+	# nvm is installed so we use that to install node v16
+	info "Using nvm to install node 16"
+	nvm install 16
 fi
 
 ###############################################################
 # node/npm
-info "Installing node v16"
-fnm install v16 >> /dev/null 2>&1
 
-which node > /dev/null
-if [[ $? != 0 ]]; then
+if ! which node > /dev/null; then
 	error "node installation failed!"
-	exit 1
-fi
-
-fnm list | egrep v16 > /dev/null
-if [[ $? != 0 ]]; then
-	error "Could not find node v16!"
 	exit 1
 fi
 
 ###############################################################
 # yarn
-which yarn > /dev/null
-if [[ $? != 0 ]]; then
+if ! which yarn > /dev/null; then
 	info "Installing yarn"
 
 	corepack enable
 	corepack prepare yarn@stable --activate
 
-	which yarn > /dev/null
-	if [[ $? != 0 ]]; then
+	if ! which yarn > /dev/null; then
 		error "yarn installation failed!"
 		exit 1
 	fi
